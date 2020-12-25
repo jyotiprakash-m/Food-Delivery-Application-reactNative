ddf5ba55c6d6259b41253936a1ff49dc878c11cd
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2020-12-25 13:17:41 +0530

    map component added

--- screens/Home.js
+++ screens/Home.js
@@ -5,10 +5,10 @@ const Home = ({navigation}) => {
 
     // Dummy Data
     const initialCurrentLocation = {
-        streetName: "Kuching",
+        streetName: "C. V. Raman Global University",
         gps: {
-            latitude: 1.5496614931250685,
-            longitude: 110.36381866919922
+            latitude: 20.22185066938992, 
+            longitude: 85.73580606090944
         }
     }
 
@@ -85,8 +85,8 @@ const Home = ({navigation}) => {
             photo: images.burger_restaurant_1,
             duration: "30 - 45 min",
             location: {
-                latitude: 1.5347282806345879,
-                longitude: 110.35632207358996,
+                latitude: 20.235582401357718, 
+                longitude: 85.74321828092879
             },
             courier: {
                 avatar: images.avatar_1,
@@ -128,8 +128,8 @@ const Home = ({navigation}) => {
             photo: images.pizza_restaurant,
             duration: "15 - 20 min",
             location: {
-                latitude: 1.556306570595712,
-                longitude: 110.35504616746915,
+                latitude: 20.21498434871911, 
+                longitude: 85.7084378639149
             },
             courier: {
                 avatar: images.avatar_2,
@@ -179,8 +179,8 @@ const Home = ({navigation}) => {
             photo: images.hot_dog_restaurant,
             duration: "20 - 25 min",
             location: {
-                latitude: 1.5238753474714375,
-                longitude: 110.34261833833622,
+                latitude: 20.250917706893595,
+                longitude: 85.75424158249604
             },
             courier: {
                 avatar: images.avatar_3,
@@ -206,8 +206,8 @@ const Home = ({navigation}) => {
             photo: images.japanese_restaurant,
             duration: "10 - 15 min",
             location: {
-                latitude: 1.5578068150528928,
-                longitude: 110.35482523764315,
+                latitude: 20.25938713958497,
+                longitude: 85.77818875531344
             },
             courier: {
                 avatar: images.avatar_4,
@@ -233,8 +233,8 @@ const Home = ({navigation}) => {
             photo: images.noodle_shop,
             duration: "15 - 20 min",
             location: {
-                latitude: 1.558050496260768,
-                longitude: 110.34743759630511,
+                latitude: 20.24200201578802,
+                longitude: 85.78664628850039
             },
             courier: {
                 avatar: images.avatar_4,
@@ -286,8 +286,8 @@ const Home = ({navigation}) => {
             photo: images.kek_lapis_shop,
             duration: "35 - 40 min",
             location: {
-                latitude: 1.5573478487252896,
-                longitude: 110.35568783282145,
+                latitude: 20.25100686123283,
+                longitude: 85.78265509310536
             },
             courier: {
                 avatar: images.avatar_1,
@@ -365,7 +365,7 @@ const Home = ({navigation}) => {
 
                         }}
                     >
-                        <Text style={{...FONTS.h3}}>{currentLocation.streetName}</Text>
+                        <Text style={{...FONTS.h3,textAlign:'center'}}>{currentLocation.streetName}</Text>
                     </View>
                 </View>
 
--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,25 +1,280 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState,useEffect } from "react";
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity
+} from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
-const OrderDelivery = () => {
+import { COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY } from "../constants"
 
-    // Render map function
+const OrderDelivery = ({ route, navigation }) => {
+
+    const [restaurant, setRestaurant] =useState(null)
+    const [streetName, setStreetName] =useState("")
+    const [fromLocation, setFromLocation] =useState(null)
+    const [toLocation, setToLocation] =useState(null)
+    const [region, setRegion] =useState(null)
+
+    const [duration, setDuration] =useState(0)
+    const [isReady, setIsReady] =useState(false)
+    const [angle, setAngle] =useState(0)
+
+    useEffect(() => {
+        let { restaurant, currentLocation } = route.params;
+
+        let fromLoc = currentLocation.gps
+        let toLoc = restaurant.location
+        let street = currentLocation.streetName
+
+        let mapRegion = {
+            latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+            longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+            latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude) * 2,
+            longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude) * 2
+        }
+
+        setRestaurant(restaurant)
+        setStreetName(street)
+        setFromLocation(fromLoc)
+        setToLocation(toLoc)
+        setRegion(mapRegion)
+
+    }, [])
+
+    // Destination marker function
+    
 
     function renderMap(){
-        return(
-            <View>
-                <Text>Lets Configure Some things</Text>
+        function destinationMarker(){
+            return(
+                <Marker coordinate={fromLocation}>
+                <View style={{
+                    height:40,
+                    width:40,
+                    borderRadius:20,
+                    alignItems:"center",
+                    justifyContent:'center',
+                    backgroundColor:COLORS.white
+                }}>
+                    <View style={{
+                        height:30,
+                        width:30,
+                        borderRadius:15,
+                        alignItems:'center',
+                        justifyContent:'center',
+                        backgroundColor:COLORS.primary
+                    }}>
+                        <Image 
+                        source={icons.pin}
+                        style={{
+                            width:25,
+                            height:25,
+                            tintColor:COLORS.white
+                        }}/>
 
+                    </View>
+                </View>
+            </Marker>
+            )
+        }
+
+        function carIcon(){
+            return(
+                <Marker
+                anchor={{x:0.5,y:0.5}}
+                flat={true}
+                coordinate={toLocation}>
+                    <View style={{
+                        width:48,
+                        height:48,
+                        borderRadius:24,
+                        backgroundColor:COLORS.primary,
+                        alignItems:"center",
+                        justifyContent:'center'
+
+                        
+                    }}>
+                        <Image
+                        source={icons.restaurant}
+                            style={{
+                                width:45,
+                                height:45,
+                                
+                            }}
+                        />
+
+                    </View>
+                    
+
+                </Marker>
+            )
+        }
+
+        return(
+            <View style={{flex:1}}>
+                <MapView 
+                style={{flex:1}} 
+                provider={PROVIDER_GOOGLE}
+                initialRegion={region}
+                >
+                    {destinationMarker()}
+
+                    {carIcon()}
+
+                </MapView>
             </View>
         )
     }
 
+    // Destinationheader function
+
+    function renderDestinationHeader() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    top: 30,
+                    left: 0,
+                    right: 0,
+                    height: 50,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        width: SIZES.width * 0.9,
+                        paddingVertical: SIZES.padding,
+                        paddingHorizontal: SIZES.padding * 2,
+                        borderRadius: SIZES.radius,
+                        backgroundColor: COLORS.white
+                    }}
+                >
+                    <Image
+                        source={icons.red_pin}
+                        style={{
+                            width: 30,
+                            height: 30,
+                            marginRight: SIZES.padding
+                        }}
+                    />
+
+                    <View style={{ flex: 1 }}>
+                        <Text style={{ ...FONTS.body3 }}>{streetName}</Text>
+                    </View>
+
+                    <Text style={{ ...FONTS.body3 }}>{Math.ceil(duration)} mins</Text>
+                </View>
+            </View>
+        )
+    }
+    function renderDeliveryInfo() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    bottom: 30,
+                    left: 0,
+                    right: 0,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <View
+                    style={{
+                        width: SIZES.width * 0.9,
+                        paddingVertical: SIZES.padding * 3,
+                        paddingHorizontal: SIZES.padding * 2,
+                        borderRadius: SIZES.radius,
+                        backgroundColor: COLORS.white
+                    }}
+                >
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        {/* Avatar */}
+                        <Image
+                            source={restaurant?.courier.avatar}
+                            style={{
+                                width: 50,
+                                height: 50,
+                                borderRadius: 25
+                            }}
+                        />
+
+                        <View style={{ flex: 1, marginLeft: SIZES.padding }}>
+                            {/* Name & Rating */}
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Text style={{ ...FONTS.h4 }}>{restaurant?.courier.name}</Text>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Image
+                                        source={icons.star}
+                                        style={{ width: 18, height: 18, tintColor: COLORS.primary, marginRight: SIZES.padding }}
+                                    />
+                                    <Text style={{ ...FONTS.body3 }}>{restaurant?.rating}</Text>
+                                </View>
+                            </View>
+
+                            {/* Restaurant */}
+                            <Text style={{ color: COLORS.darkgray, ...FONTS.body4 }}>{restaurant?.name}</Text>
+                        </View>
+                    </View>
+
+                    {/* Buttons */}
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            marginTop: SIZES.padding * 2,
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                height: 50,
+                                marginRight: 10,
+                                backgroundColor: COLORS.primary,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                borderRadius: 10
+                            }}
+                            onPress={() => navigation.navigate("Home")}
+                        >
+                            <Text style={{ ...FONTS.h4, color: COLORS.white }}>Call</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                height: 50,
+                                backgroundColor: COLORS.secondary,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                borderRadius: 10
+                            }}
+                            onPress={() => navigation.goBack()}
+                        >
+                            <Text style={{ ...FONTS.h4, color: COLORS.white }}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            </View>
+        )
+    }
+
+    
+
 
     return (
-        <View style={{flex:1}} >
+        <View style={{ flex: 1 }}>
             {renderMap()}
+            {renderDestinationHeader()}
+            {renderDeliveryInfo()}
         </View>
     )
 }
 
-export default OrderDelivery
+export default OrderDelivery;
